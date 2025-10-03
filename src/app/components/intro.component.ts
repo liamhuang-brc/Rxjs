856e6244665e2b8bfa61520853a1d50be4d73391
liamhuang-brc <liam.huang@brainridgeconsulting.com>
2025-10-03 14:07:19 -0400

    chore: Upgrade Angular from 17.0.0 to 18.0.0

--- src/app/components/intro.component.ts
+++ src/app/components/intro.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { animate, style, transition, trigger } from '@angular/animations';
-import { HttpClient } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-intro',
